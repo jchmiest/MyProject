403337727276b8384ad290f92cc637a43befd7b6
Justyna Chmiest <jchmiest4@gmail.com>
2020-02-08 10:42:58 +0100

    initial commit

---
+++ src/main/java/pl/sda/carmanager/Main.java
@@ -0,0 +1,7 @@
+package pl.sda.carmanager;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My project");
+    }
+}
---
+++ src/test/java/pl/sda/carmanager/CarmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.carmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
