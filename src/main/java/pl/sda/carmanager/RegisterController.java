68a8f333e7764bd786584985eea8688efab4aa76
Justyna Chmiest <jchmiest4@gmail.com>
2020-02-16 14:13:35 +0100

    initial commit

--- src/main/java/pl/sda/carmanager/RegisterController.java
+++ src/main/java/pl/sda/carmanager/RegisterController.java
@@ -27,13 +27,30 @@ public class RegisterController {
 
     @GetMapping("/show")//wyswietli to co dodam,wysiwtela formularz carform1
     public ModelAndView getShowRegister() {
-        List<FuelEntity> fuelentries = fuelRepository.findAll(Sort.by(Sort.Direction.ASC,"date"));
+        List<FuelEntity> fuelentries = fuelRepository.findAll(Sort.by(Sort.Direction.ASC, "date"));
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("registerview");//wpisujemy co ma wyswietlic
         modelAndView.getModel().put("register", fuelentries);
         //tworzy sie nowy model, fuelRepository-mamy tam info o licznik itp, findAll-wyszukujemy wszystko
         //k-klucz jest dowolny ,fuelentries-zwraca wszystko co mam w car repository
 
-        return modelAndView;
+        //ascenting-sortowanie rosnace
+        Integer result= 0;
+        for (int i = 0; i < fuelentries.size(); i++) {//lista
+
+          FuelEntity entity1=   fuelentries.get(i); //pobranie konkretnej rzeczy w liscie,index 0
+          FuelEntity entity2=   fuelentries.get(i+1);// nastepny counter,pobranie,index 1,poranie obiektu z listy
+            Integer counter1 = entity1.getCounter();////dzialanie na obiekcie
+            Integer counter2 = entity2.getCounter();
+            result=counter1-counter2;
+            }
+
+        modelAndView.getModel().put("result",result); //dodanie do widoku
+
+
+
+
+            return modelAndView;
+        }
     }
-}
+
